0b0635a455bc3454722915704bd1772687598466
Bambe058 <abebruinsma@hotmail.com>
2024-02-04 14:18:17 +0100

    Update startup.sh

--- OS2/Linux/week 5/startup.sh
+++ OS2/Linux/week 5/startup.sh
@@ -26,6 +26,7 @@ ssh-add /home/localadmin/.ssh/id_rsa_docker
 
 #inhoud van de hosts
 hosts_inhoud="[servers]
+wordpress_host ansible_user=localhost
 wordpress_host ansible_host=10.6.0.100
 wordpress_host ansible_ssh_private_key_file=/home/localadmin/.ssh/id_rsa_wordpress
 wordpress_host ansible_python_interpreter=/usr/bin/python3
